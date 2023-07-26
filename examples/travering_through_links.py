ad21e953fcd5cfe52104418b062043a217e2dc73
leyuskckiran1510 <kdhakal1510@gmail.com>
2023-07-26 17:18:52 +0545

    example added

--- examples/travering_through_links.py
+++ examples/travering_through_links.py
@@ -3,5 +3,10 @@ from H2S.h2s import Har
 harFile = "./HAR_FILE.har"
 
 x = Har(harFile)
-for i in x.entries():
-    content = i.response.content
+pages = x.pages()
+requests_list = x.entries()
+for i in requests_list:
+    print(i.request.url)
+    print(i.response.content)
+    print(i.request.query)
+    print(i.request.url)
